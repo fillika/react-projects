c2dd0b7a87a8b8dca4ddf4ae33a1e60dcecb1cb9
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-11-29 22:00:51 +0300

    Did drag circles

--- canvas-tutorial/src/components/App/App.tsx
+++ canvas-tutorial/src/components/App/App.tsx
@@ -1,5 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
+
+const CONSTANTS = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 class Circle {
   ctx: CanvasRenderingContext2D;
@@ -26,25 +31,26 @@ class Circle {
   }
 
   update() {
+    this.ctx.clearRect(0, 0, CONSTANTS.width, CONSTANTS.height);
+
+    // if (this.x + this.radius > CONSTANTS.width || this.x - this.radius < 0) {
+    //   this.dx = -this.dx;
+    // }
+    //
+    // if (this.y + this.radius > CONSTANTS.height || this.y - this.radius < 0) {
+    //   this.dy = -this.dy;
+    // }
+    //
+    this.x += 15;
+    this.y += 15;
     this.draw();
-
-    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
-      this.dx = -this.dx;
-    }
-
-    if (this.y + this.radius > window.innerHeight / 2 || this.y - this.radius < 0) {
-      this.dy = -this.dy;
-    }
-
-    this.x += this.dx;
-    this.y += this.dy;
   }
 }
 
 function createArrayOfCircles(array: Circle[], ctx: CanvasRenderingContext2D) {
-  const radius = 45;
-  const x = Math.random() * (window.innerWidth - radius * 2) + radius;
-  const y = Math.random() * (window.innerHeight / 2 - radius * 2) + radius;
+  const radius = 15;
+  const x = Math.random() * (CONSTANTS.width - radius * 2) + radius;
+  const y = Math.random() * (CONSTANTS.height - radius * 2) + radius;
   const dx = (Math.random() - 0.5) * 2;
   const dy = (Math.random() - 0.5) * 2;
 
@@ -54,37 +60,36 @@ function createArrayOfCircles(array: Circle[], ctx: CanvasRenderingContext2D) {
 
 function App() {
 
-  /** Создаю 2 рефа.
+  console.log('render');
+  /** Создаю 3 рефа.
    * contextRef - нужен для того, чтобы Я привязал к нему реальный контекст canvas единожды
    * И потом спокойно обращался к нему
    */
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
   const circleArray = useRef<Circle[] | null>(null);
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    if (contextRef.current) {
-      contextRef.current.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-      if (circleArray.current !== null) {
-        circleArray.current.forEach(circle => {
-          circle.update();
-        });
-      }
-    }
-  }
-
-  console.log('RENDER');
+  const [isDraggable, setDraggable] = useState(false);
 
   // Хук при инициализации
   useEffect(() => {
+    function animate() {
+      requestAnimationFrame(animate);
+
+      if (contextRef.current) {
+        contextRef.current.clearRect(0, 0, CONSTANTS.width, CONSTANTS.height);
+
+      }
+
+      if (circleArray.current) {
+        circleArray.current.forEach(circle => circle.draw());
+      }
+    }
+
     const canvas = canvasRef.current;
 
     if (canvas) {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight / 2;
+      canvas.width = CONSTANTS.width;
+      canvas.height = CONSTANTS.height;
 
       const context = canvas.getContext('2d');
 
@@ -92,19 +97,44 @@ function App() {
         contextRef.current = context;
         circleArray.current = []; // массив будущих кругов
 
-        for (let i = 0; i < 100; i++) {
+        // Создаем объекты
+        for (let i = 0; i < 50; i++) {
           createArrayOfCircles(circleArray.current, contextRef.current);
         }
 
         animate();
       }
     }
-  });
+  }, []);
+
+  const mouseDown = () => setDraggable(true);
+  const mouseUp = () => setDraggable(false);
+
+  const mouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (isDraggable) {
+      const { pageX, pageY } = event;
+
+      if (circleArray.current !== null) {
+
+        for (let k = 0; k < circleArray.current.length; k++) {
+          const circle = circleArray.current[k];
+          const { x, y, radius } = circle;
+
+          if ((pageX - x <= radius && pageX - x >= -radius) && (pageY - y <= radius && pageY - y >= -radius)) {
+            circle.x = pageX;
+            circle.y = pageY;
+            break;
+          }
+        }
+      }
+    }
+  };
 
   return (
     <div className="App">
       <div className="app-wrapper">
-        <canvas ref={ canvasRef } className="canvas"/>
+        <h1>Welcome to CANVAS</h1>
+        <canvas onMouseMove={ mouseMove } onMouseDown={ mouseDown } onMouseUp={ mouseUp } ref={ canvasRef } className="canvas"/>
       </div>
     </div>
   );
